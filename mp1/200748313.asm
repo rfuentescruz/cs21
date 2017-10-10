cd03aa11d53b54d67243d6190184c8e7df28a475
Rolando Cruz <rolando.cruz21@gmail.com>
2017-10-11 00:48:46 +0800

    [mp1] Implemented working clockwise rotations

--- mp1/200748313.asm
+++ mp1/200748313.asm
@@ -1,49 +1,76 @@
 .text
+.macro read_string(%label, %len)
+	la	$a0, %label
+	add	$a1, $zero, %len
+	li	$v0, 8
+	syscall
+.end_macro
+
+.macro read_int(%reg)
+	li	$v0, 5
+	syscall
+	move	%reg, $v0
+.end_macro
+
+.macro print_string(%label)
+	la	$a0, %label
+	li	$v0, 4
+	syscall
+.end_macro
+
 .macro printc(%c)
 	add	$a0, $zero, %c
 	li	$v0, 11
 	syscall
 .end_macro
 
-.macro call_swap(%gridA, %gridB, %rowA, %colA, %rowB, %colB)
+.macro print_int(%n)
+	add	$a0, $zero, %n
+	li	$v0, 1
+	syscall
+.end_macro
+
+.macro call_swap(%gridA, %gridB, %dimA, %dimB)
 	addu	$a0, $zero, %gridA
 	addu	$a1, $zero, %gridB
-	add	$t8, $zero, %rowA
+	add	$t8, $zero, %dimA
 	sb	$t8, -1($sp)
-	add	$t8, $zero, %colA
+	add	$t8, $zero, %dimB
 	sb	$t8, -2($sp)
-	add	$t8, $zero, %rowB
-	sb	$t8, -3($sp)
-	add	$t8, $zero, %colB
-	sb	$t8, -4($sp)
 	jal	swap
 .end_macro
 
-main:	la	$a0, cube
-	li	$a1, 55
-	li	$v0, 8
-	syscall
+.macro get_grid_address(%reg, %grid)
+	add	%reg, $zero, %grid
+	mul	%reg, %reg, 9
+	la	%reg, cube(%reg)
+.end_macro
 
-	li	$v0, 5
-	syscall
-	move	$s0, $v0
+.macro get_row_address(%reg, %grid, %row)
+	get_grid_address(%reg, %grid)
+	add	$t8, $zero, %row
+	mul	$t8, $t8, 3
+	add	%reg, %reg, $t8
+.end_macro
 
-	la	$a0, cube
-	li	$v0, 4
-	syscall
+.macro get_column_address(%reg, %grid, %column)
+	get_grid_address(%reg, %grid)
+	add	%reg, %reg, %column
+.end_macro
 
-	move	$a0, $s0
-	li	$v0, 1
-	syscall
+################################################################################
+# main
+################################################################################
+main:	read_string(cube, 55)
+	print_string(cube)
+	printc('\n')
 
-	la	$a0, actions
-	li	$a1, 1001
-	li	$v0, 8
-	syscall
+	read_int($s0)
+	print_int($s0)
+	printc('\n')
 
-	la	$a0, actions
-	li	$v0, 4
-	syscall
+	read_string(actions, 1001)
+	print_string(actions)
 
 	la	$s1, actions
 loop:	lb	$s2, ($s1)		# Load c
@@ -51,8 +78,8 @@ loop:	lb	$s2, ($s1)		# Load c
 	beq	$s2, 10, end		# if c == '\n', end
 
 	lb	$s3, 1($s1)		# Load c + 1
-	li	$a1, 0			# reverse = false
-	bne	$s3, 39, next		# if (c + 1) != "'", do action immediately
+	li	$a1, 0			# Set reverse = false initially
+	bne	$s3, '\'', next		# if (c + 1) != "'", do action immediately
 	li	$a1, 1			# reverse = true
 	add	$s1, $s1, 1		# Skip next char
 next:	move	$a0, $s2
@@ -62,25 +89,71 @@ next:	move	$a0, $s2
 
 end:	li	$v0, 10
 	syscall
+################################################################################
+# end main
+################################################################################
 
+
+################################################################################
+# action(c) - Perform a Rubik's cube rotation / action
+#
+# Params:
+#   $a0 - The uppercase character representing the move
+################################################################################
 action:	subu	$sp, $sp, 32
-	sw	$ra, 28($sp)
+	sw	$s0, 28($sp)
+	sw	$s1, 24($sp)
+	sw	$s2, 20($sp)
+	sw	$s3, 16($sp)
+	sw	$ra, ($sp)
 
-	move	$t0, $a0
-	printc($a0)
-	printc(':')
-	printc(' ')
+	move	$s0, $a0
 
-	move	$a0, $t0
-	jal	get_index
-	call_swap($v0, $v0, 0, -1, 2, -1)
+	move	$a0, $s0
+	jal	get_index			# v = get_index($a0)
+	move	$s1, $v0			# $t0 = $a0
 
+	move	$a0, $s0
+	jal	rotate
+
+	mul	$t0, $s1, 4
+	la	$s2, scramble($t0)
+
+	li	$s3, 0
+scramble_loop:
+	add	$t5, $s2, $s3
+
+	lb	$t0, ($t5)
+	srl	$t0, $t0, 4
+	lb	$t1, 1($t5)
+	srl	$t1, $t1, 4
+
+	lb	$t2, ($t5)
+	and	$t2, $t2, 0x0F
+	lb	$t3, 1($t5)
+	and	$t3, $t3, 0x0F
+	call_swap($t0, $t1, $t2, $t3)
+
+	add	$s3, $s3, 1
+	blt	$s3, 3, scramble_loop
+
+	print_string(cube)
 	printc('\n')
 
-	lw	$ra, 28($sp)
+	lw	$s0, 28($sp)
+	lw	$s1, 24($sp)
+	lw	$s2, 20($sp)
+	lw	$s3, 16($sp)
+	lw	$ra, ($sp)
 	addu	$sp, $sp, 32
 	jr	$ra
 
+################################################################################
+# rotate(face) - Rotate a face
+#
+# Params:
+#   $a0 - The uppercase character representing the move
+################################################################################
 rotate:					# rotate(face)
 	subu	$sp, $sp, 32
 	sw	$ra, 28($sp)
@@ -114,7 +187,6 @@ lr1: 	lb	$t3, ($t1)		# c = *g
 
 	add	$t0, $s0, 9
 rl:	lb	$t1, ($s0)		# c = *cube
-	printc($t1)
 	add	$s0, $s0, 1		# cube++
 	blt	$s0, $t0, rl		# if a < b, loop rl
 
@@ -144,59 +216,85 @@ face_u:	li	$v0, 4
 face_d:	li	$v0, 5
 q:	jr	$ra
 
+################################################################################
+# copy(src_address, dest_address) - Copy a grid at $a0, to $a1
+################################################################################
 copy:	li	$t0, 0
 	move	$t1, $a0
 	move	$t2, $a1
-lc:	lb	$t3, ($t1)
+
+loop_copy:
+	lb	$t3, ($t1)
 	sb	$t3, ($t2)
 	add	$t0, $t0, 1
 	add	$t1, $t1, 1
 	add	$t2, $t2, 1
-	blt	$t0, 9, lc
+	blt	$t0, 9, loop_copy
 	jr	$ra
+################################################################################
+# end copy
+################################################################################
 
+################################################################################
+# swap(grid_indexA, grid_indexB, row_or_col_A, row_or_col_B)
+#
+# Swap rows or columns of one grid with another.
+################################################################################
 swap:	subu	$sp, $sp, 8
-	lb	$t0, 7($sp)		# rowA
-	lb	$t1, 6($sp)		# colA
-	lb	$t2, 5($sp)		# rowB
-	lb	$t3, 4($sp)		# colB
+	lb	$t0, 7($sp)		# row_or_col_A
+	lb	$t1, 6($sp)		# row_or_col_B
 	sw	$ra, ($sp)
 
-	mul	$t4, $a0, 9
-	la	$t4, cube($t4)	# gridA
-
-	mul	$t5, $a1, 9
-	la	$t5, cube($t5)	# gridB
-
-	bltz	$t0, swap1
+	mul	$t2, $a0, 9
+	la	$t2, cube($t2)	# gridA
+	bge	$t0, 3, use_colsA
 	mul	$t0, $t0, 3
-	addu	$t4, $t4, $t0		# rowA of gridA
+	add	$t2, $t2, $t0
+	li	$t0, 1
+	b	choose_A_end
+use_colsA:
+	sub	$t0, $t0, 3
+	add	$t2, $t2, $t0
+	li	$t0, 3
+choose_A_end:
 
-	mul	$t2, $t2, 3
-	addu	$t5, $t5, $t2		# rowB of gridB
+	mul	$t3, $a1, 9
+	la	$t3, cube($t3)	# gridB
+	bge	$t1, 3, use_colsB
+	mul	$t1, $t1, 3
+	add	$t3, $t3, $t1
+	li	$t1, 1
+	b	choose_B_end
+use_colsB:
+	sub	$t1, $t1, 3
+	add	$t3, $t3, $t1
+	li	$t1, 3
+choose_B_end:
 
-	li	$t6, 3
+	li	$t4, 3
 swap_loop:
-	lb	$t0, ($t4)
-	lb	$t1, ($t5)
-	sb	$t0, ($t5)
-	sb	$t1, ($t4)
-	sub	$t6, $t6, 1
-	add	$t4, $t4, 1
-	add	$t5, $t5, 1
-	bgtz	$t6, swap_loop
+	lb	$t5, ($t2)
+	lb	$t6, ($t3)
+
+	sb	$t5, ($t3)
+	sb	$t6, ($t2)
+
+	add	$t2, $t2, $t0
+	add	$t3, $t3, $t1
+	sub	$t4, $t4, 1	
+	bgtz	$t4, swap_loop
 
 	lw	$ra, ($sp)
 	addu	$sp, $sp, 8
 	jr	$ra
 
 .data
-scramble:	.word 0x10453252 # F
-		.word 0x02522042 # R
-		.word 0x12553043 # B
-		.word 0x00402250 # L
-		.word 0x03132333 # U
-		.word 0x05352515 # D
+scramble:	.word 0x50354213 # F
+		.word 0x45235505 # R
+		.word 0x40335215 # B
+		.word 0x53254303 # L
+		.word 0x30201000 # U
+		.word 0x12223202 # D
 cube:		.space 54
 		.align 2
 grid:		.space 9
