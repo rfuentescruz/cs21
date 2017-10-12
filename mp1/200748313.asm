d51f165da39c90ef779ecd37001ae6518e2bf0dd
Rolando Cruz <rolando.cruz21@gmail.com>
2017-10-12 13:08:32 +0800

    [mp1] Implement horizontal and vertical rotations correctly

--- mp1/200748313.asm
+++ mp1/200748313.asm
@@ -30,47 +30,18 @@
 	syscall
 .end_macro
 
-.macro call_swap(%gridA, %gridB, %dimA, %dimB)
-	addu	$a0, $zero, %gridA
-	addu	$a1, $zero, %gridB
-	add	$t8, $zero, %dimA
-	sb	$t8, -1($sp)
-	add	$t8, $zero, %dimB
-	sb	$t8, -2($sp)
-	jal	swap
-.end_macro
-
-.macro get_grid_address(%reg, %grid)
-	add	%reg, $zero, %grid
-	mul	%reg, %reg, 9
-	la	%reg, cube(%reg)
-.end_macro
-
-.macro get_row_address(%reg, %grid, %row)
-	get_grid_address(%reg, %grid)
-	add	$t8, $zero, %row
-	mul	$t8, $t8, 3
-	add	%reg, %reg, $t8
-.end_macro
-
-.macro get_column_address(%reg, %grid, %column)
-	get_grid_address(%reg, %grid)
-	add	%reg, %reg, %column
+.macro call_move_cell(%to, %from)
+	add	$a0, $zero, %to
+	add	$a1, $zero, %from
+	jal	move_cell
 .end_macro
 
 ################################################################################
 # main
 ################################################################################
 main:	read_string(cube, 55)
-	print_string(cube)
-	printc('\n')
-
 	read_int($s0)
-	print_int($s0)
-	printc('\n')
-
 	read_string(actions, 1001)
-	print_string(actions)
 
 	la	$s1, actions
 loop:	lb	$s2, ($s1)		# Load c
@@ -82,12 +53,15 @@ loop:	lb	$s2, ($s1)		# Load c
 	bne	$s3, '\'', next		# if (c + 1) != "'", do action immediately
 	li	$a1, -1			# direction = counter-clockwise
 	add	$s1, $s1, 1		# Skip next char
-next:	move	$a0, $s2
+next:
+	move	$a0, $s2
 	jal	action			# action(c, direction)
 	add	$s1, $s1, 1
 	b	loop
 
-end:	li	$v0, 10
+end:
+	print_string(cube)
+	li	$v0, 10
 	syscall
 
 ################################################################################
@@ -123,7 +97,18 @@ translate_h:
 	li	$a0, 'D'
 	li	$a1, 1
 	jal	rotate			# rotate(v)
-	b	swap_pair_start
+	la	$a0, cube
+	la	$a1, grid
+	jal	copy
+	call_move_cell(5, 32)
+	call_move_cell(32, 23)
+	call_move_cell(23, 14)
+	call_move_cell(14, 5)
+	call_move_cell(4, 31)
+	call_move_cell(31, 22)
+	call_move_cell(22, 13)
+	call_move_cell(13, 4)
+	b	action_end
 
 translate_v:
 	li	$a0, 'R'
@@ -132,53 +117,25 @@ translate_v:
 	li	$a0, 'L'
 	li	$a1, -1
 	jal	rotate			# rotate(v)
-	b	swap_pair_start
+	la	$a0, cube
+	la	$a1, grid
+	jal	copy
+	call_move_cell(37, 1)
+	call_move_cell(1, 46)
+	call_move_cell(46, 25)
+	call_move_cell(25, 37)
+	call_move_cell(40, 4)
+	call_move_cell(4, 49)
+	call_move_cell(49, 22)
+	call_move_cell(22, 40)
+	b	action_end
 
 face_rotate:
 	move	$a0, $s0
 	move	$a1, $s1
 	jal	rotate			# rotate(v)
-	b	swap_pair_loop_end
-swap_pair_start:
-	mul	$t0, $s4, 4
-	la	$s2, adjacents($t0)	# Get side rotation map
 
-	li	$s3, 3			# Start with last swap pair
-	bltz	$s1, swap_pair_loop
-	li	$s3, 0			# Start w/ first swap pair if clockwise
-
-swap_pair_loop:
-	add	$t4, $s2, $s3
-	add	$t5, $t4, $s1
-
-	lb	$t0, ($t4)		# Get gridA index from pair
-	srl	$t0, $t0, 4
-	lb	$t1, ($t5)		# Get gridB index from pair
-	srl	$t1, $t1, 4
-
-	lb	$t2, ($t4)		# Get row_or_colA from pair
-	and	$t2, $t2, 0x0F
-	lb	$t3, ($t5)		# Get row_or_colB from pair
-	and	$t3, $t3, 0x0F
-
-	call_swap($t0, $t1, $t2, $t3)	# Swap row_or_colA with row_or_colB
-
-	add	$s3, $s3, $s1		# Move to next swap pair
-	blez	$s3, swap_pair_loop_end
-	bge	$s3, 3, swap_pair_loop_end
-	b	swap_pair_loop
-
-swap_pair_loop_end:
-	printc($s0)
-	bltz	$s1, ccw_sw
-	printc(' ')
-	b	np
-ccw_sw:	printc('\'')
-np:	printc(':')
-	printc(' ')
-	print_string(cube)
-	printc('\n')
-
+action_end:
 	lw	$s0, 28($sp)
 	lw	$s1, 24($sp)
 	lw	$s2, 20($sp)
@@ -202,143 +159,56 @@ rotate:					# rotate(face)
 	sw	$s1, 20($sp)
 	sw	$s2, 16($sp)
 	sw	$s3, 12($sp)
-	sw	$s4, 8($sp)
-	sw	$s5, 4($sp)
-	sw	$s6, ($sp)
 
-	move	$s1, $a0		# face = args[0]
-	move	$s2, $a1		# direction = args[1]
+	move	$s0, $a0		# face = args[0]
+	move	$s1, $a1		# direction = args[1]
 
-	la	$s0, cube		# a = &cube
-	move	$a0, $s0
+	la	$a0, cube
 	la	$a1, grid
 	jal	copy
 
-	move	$a0, $s1
+	move	$a0, $s0
 	jal	get_index		# get_index(face)
-	move	$s1, $v0		# i = get_index(face)
-	mul	$s1, $s1, 9		# i = i * 9
-	add	$s0, $s0, $s1		# a += i
-	la	$s4, grid($s1)		# Determine where to copy from grid
+	move	$s0, $v0		# i = get_index(face)
+	mul	$s0, $s0, 9		# i = i * 9
 
-	move	$t0, $s1
-	move	$t1, $s1
-	bgtz	$s2, clockwise
-	add	$t0, $t0, 6
+	move	$s2, $s0
+	move	$s3, $s0
+	bgtz	$s1, clockwise
+	add	$s2, $s2, 6
 	b	rotate_loop
 clockwise:
-	add	$t0, $t0, 2
+	add	$s2, $s2, 2
+
 rotate_loop:
-	lb	$t2, grid($t1)
-	sb	$t2, cube($t0)
+	move	$a0, $s2
+	move	$a1, $s3
+	jal	move_cell
 
-	mul	$t3, $t1, 4
-	lw	$t3, turn_pairs($t3)
-	and	$t3, $t3, 0x0000FF00
-	srl	$t3, $t3, 8
-	bgt	$t3, 53, no_pair
-	lb	$t2, grid($t3)
-	
-	mul	$t3, $t0, 4
-	lw	$t3, turn_pairs($t3)
-	and	$t3, $t3, 0x0000FF00
-	srl	$t3, $t3, 8
-	bgt	$t3, 53, no_pair
-	sb	$t2, cube($t3)
+	mul	$t0, $s1, 3		# Move row up or down
+	add	$s2, $s2, $t0
+	add	$s3, $s3, 1		# Move copy target to next byte
 
-	mul	$t3, $t1, 4
-	lw	$t3, turn_pairs($t3)
-	and	$t3, $t3, 0x000000FF
-	bgt	$t3, 53, no_pair
-	lb	$t2, grid($t3)
-	
-	mul	$t3, $t0, 4
-	lw	$t3, turn_pairs($t3)
-	and	$t3, $t3, 0x000000FF
-	bgt	$t3, 53, no_pair
-	sb	$t2, cube($t3)
+	sub	$t0, $s3, $s0		# Check progress
+	rem	$t0, $t0, 3
+	bnez	$t0, rotate_loop
 
-no_pair:
-	mul	$t2, $s2, 3		# Move row up or down
-	add	$t0, $t0, $t2
-	add	$t1, $t1, 1		# Move copy target to next byte
+	mul	$t0, $s1, 9		# Reset back row changes
+	neg	$t0, $t0
+	add	$s2, $s2, $t0
 
-	sub	$t2, $t1, $s1		# Check progress
-	rem	$t2, $t2, 3
-	bnez	$t2, rotate_loop
-
-	mul	$t2, $s2, 9		# Reset back row changes
-	neg	$t2, $t2
-	add	$t0, $t0, $t2
-
-	mul	$t2, $s2, 1		# Adjust column
-	neg	$t2, $t2
-	add	$t0, $t0, $t2
-	sub	$t2, $t1, $s1
-	blt	$t2, 9, rotate_loop
+	mul	$t0, $s1, 1		# Adjust column
+	neg	$t0, $t0
+	add	$s2, $s2, $t0
+	sub	$t0, $s3, $s0
+	blt	$t0, 9, rotate_loop
 
 	lw	$ra, 28($sp)
 	lw	$s0, 24($sp)
 	lw	$s1, 20($sp)
-	lw	$s2, 16($sp)
-	lw	$s3, 12($sp)
-	lw	$s4, 8($sp)
-	lw	$s5, 4($sp)
-	lw	$s6, ($sp)
+	sw	$s2, 16($sp)
+	sw	$s3, 12($sp)
 	addu	$sp, $sp, 32
-	jr	$ra
-
-################################################################################
-# swap(grid_indexA, grid_indexB, row_or_col_A, row_or_col_B)
-#
-# Swap rows or columns of one grid with another.
-################################################################################
-swap:	subu	$sp, $sp, 8
-	lb	$t0, 7($sp)		# row_or_col_A
-	lb	$t1, 6($sp)		# row_or_col_B
-	sw	$ra, ($sp)
-
-	mul	$t2, $a0, 9		# grid_offsetA = grid_indexA * 9
-	la	$t2, cube($t2)		# gridA = &(cube + grid_offsetA)
-	bge	$t0, 3, use_colsA	# Do we use columns or rows for gridA?
-	mul	$t0, $t0, 3		# Use rows: Calculate row offset
-	add	$t2, $t2, $t0		# gridA = gridA + row_offset
-	li	$t0, 1			# incrementA = 1
-	b	choose_A_end
-use_colsA:
-	sub	$t0, $t0, 3		# Use columns: Calculate column index
-	add	$t2, $t2, $t0		# gridA = gridA + column_index
-	li	$t0, 3			# incrementA = 3
-choose_A_end:
-
-	mul	$t3, $a1, 9
-	la	$t3, cube($t3)		# gridB
-	bge	$t1, 3, use_colsB
-	mul	$t1, $t1, 3
-	add	$t3, $t3, $t1
-	li	$t1, 1
-	b	choose_B_end
-use_colsB:
-	sub	$t1, $t1, 3
-	add	$t3, $t3, $t1
-	li	$t1, 3
-choose_B_end:
-
-	li	$t4, 3			# i = 3
-swap_loop:
-	lb	$t5, ($t2)		# a = &gridA
-	lb	$t6, ($t3)		# b = &gridB
-
-	sb	$t5, ($t3)		# *gridA = b
-	sb	$t6, ($t2)		# *gridB = a
-
-	add	$t2, $t2, $t0		# gridA += incrementA
-	add	$t3, $t3, $t1		# gridB += incrementB
-	sub	$t4, $t4, 1		# --i
-	bgtz	$t4, swap_loop		# loop if i > 0
-
-	lw	$ra, ($sp)
-	addu	$sp, $sp, 8
 	jr	$ra
 
 ################################################################################
@@ -388,16 +258,42 @@ loop_copy:
 	blt	$t0, 54, loop_copy
 	jr	$ra
 
+move_cell:
+	move	$t0, $a0
+	move	$t1, $a1
+
+	lb	$t2, grid($t1)
+	sb	$t2, cube($t0)
+
+	mul	$t3, $t1, 4
+	lw	$t3, turn_pairs($t3)
+	and	$t3, $t3, 0x0000FF00
+	srl	$t3, $t3, 8
+	bgt	$t3, 53, no_pair
+	lb	$t2, grid($t3)
+
+	mul	$t3, $t0, 4
+	lw	$t3, turn_pairs($t3)
+	and	$t3, $t3, 0x0000FF00
+	srl	$t3, $t3, 8
+	bgt	$t3, 53, no_pair
+	sb	$t2, cube($t3)
+
+	mul	$t3, $t1, 4
+	lw	$t3, turn_pairs($t3)
+	and	$t3, $t3, 0x000000FF
+	bgt	$t3, 53, no_pair
+	lb	$t2, grid($t3)
+
+	mul	$t3, $t0, 4
+	lw	$t3, turn_pairs($t3)
+	and	$t3, $t3, 0x000000FF
+	bgt	$t3, 53, no_pair
+	sb	$t2, cube($t3)
+no_pair:
+	jr	$ra
 
 .data
-adjacents:	.word 0x50354213 # F
-		.word 0x45235505 # R
-		.word 0x40335215 # B
-		.word 0x53254303 # L
-		.word 0x30201000 # U
-		.word 0x12223202 # D
-		.word 0x11213101 # H
-		.word 0x24544404 # V
 turn_pairs:	.word 0x00001D2A   # ['0', '29', '42']
 		.word 0x00012BFF   # ['1', '43']
 		.word 0x00022C09   # ['2', '44', '9']
@@ -452,10 +348,6 @@ turn_pairs:	.word 0x00001D2A   # ['0', '29', '42']
 		.word 0x00331A21   # ['51', '26', '33']
 		.word 0x003419FF   # ['52', '25']
 		.word 0x00351118   # ['53', '17', '24']
-
-
-inversions:	.word 0x01000100
-		.word 0x00010000
 cube:		.space 54
 		.align 2
 grid:		.space 54
